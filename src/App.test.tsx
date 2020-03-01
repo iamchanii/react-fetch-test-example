afed1f062ada7e420ddcdb72a6300092bac97dcd
iamchanii <iamchanii.0310@gmail.com>
2020-03-01 21:59:00 +0900

    test(dog): Add test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
+import { render, waitForElement } from '@testing-library/react';
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { act } from 'react-dom/test-utils';
+import App, { RANDOM_DOG_IMAGE_URL } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders random toy poodle image', async () => {
+  let _resolve: any;
+
+  fetchMock.mockResponse(request => {
+    if (request.url === RANDOM_DOG_IMAGE_URL) {
+      return new Promise(resolve => {
+        _resolve = resolve;
+      });
+    }
+
+    return Promise.resolve('');
+  });
+
+  const { getByAltText } = render(<App />);
+
+  act(() => {
+    _resolve(
+      JSON.stringify({
+        message: 'https://dog.jpg',
+        result: 'success',
+      }),
+    );
+  });
+
+  const toyPoodle = await waitForElement(() => getByAltText(/toy poodle/i));
+
+  expect(toyPoodle).toHaveAttribute('src', 'https://dog.jpg');
 });
